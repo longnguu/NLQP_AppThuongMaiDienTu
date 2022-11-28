3dac942f6e45f4124334e9b22857ddedc186c15f
longnguu <levanlong29062002@gmail.com>
2022-11-28 18:14:21 +0700

    Chat

--- app/src/main/java/com/example/appthuongmaidientu/activity/MainActivity.java
+++ app/src/main/java/com/example/appthuongmaidientu/activity/MainActivity.java
@@ -2,15 +2,27 @@ package com.example.appthuongmaidientu.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.appthuongmaidientu.R;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView iconMess,iconCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iconMess = (ImageView) findViewById(R.id.topnavMess);
+        iconMess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,UIMessenger.class);
+                startActivity(intent);
+            }
+        });
     }
 }
