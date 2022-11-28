97e2606599bf910e3ad30e4080e4326165845e3e
longnguu <levanlong29062002@gmail.com>
2022-11-28 18:54:35 +0700

    ket noi firebase

--- app/src/main/java/com/example/appthuongmaidientu/activity/Signup.java
+++ app/src/main/java/com/example/appthuongmaidientu/activity/Signup.java
@@ -1,10 +1,28 @@
 package com.example.appthuongmaidientu.activity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.example.appthuongmaidientu.Class.User;
 import com.example.appthuongmaidientu.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Signup extends AppCompatActivity {
 
@@ -12,5 +30,6 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
     }
 }
